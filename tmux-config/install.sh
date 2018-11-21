7f96c7be9a14b6af466198ece985728741458b50
Tom Dunlap <apocryphalauthor@gmail.com>
2018-11-21 16:49:54 +0000

    Fix installing tmux plugins

--- tmux-config/install.sh
+++ tmux-config/install.sh
@@ -6,6 +6,7 @@ cp "$conf_file" "$current_conf_file"
 
 rm -rf $HOME/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+export TMUX_PLUGIN_MANAGER_PATH=$HOME/.tmux/plugins
 ~/.tmux/plugins/tpm/bin/install_plugins
 ~/.tmux/plugins/tpm/bin/update_plugins all
 ~/.tmux/plugins/tpm/bin/clean_plugins
