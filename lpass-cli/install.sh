19b4b6017de16d0712e8e694072a0580ab8b5442
Tom Dunlap <apocryphalauthor@gmail.com>
2018-12-02 22:31:12 -0800

    Install compatible version of libssl for lastpass
    In Debian if you don't install libssl1.0-dev then lpass segfaults.

--- lpass-cli/install.sh
+++ lpass-cli/install.sh
@@ -4,8 +4,14 @@
 
 set -eu
 
+if [ "$(lsb_release -is)" = "Debian" ]; then
+  lib_ssl_dev="libssl1.0-dev"
+else
+  lib_ssl_dev="libssl-dev"
+fi
+
 sudo apt-get update
-sudo apt-get install -y openssl libcurl4-openssl-dev libxml2 libssl1.0-dev libxml2-dev pinentry-curses xclip cmake build-essential pkg-config
+sudo apt-get install -y curl openssl libcurl4-openssl-dev libxml2 $lib_ssl_dev libxml2-dev pinentry-curses xclip cmake build-essential pkg-config
 cd /tmp
 curl -sL https://github.com/lastpass/lastpass-cli/releases/download/v1.3.1/lastpass-cli-1.3.1.tar.gz > lastpass-cli-1.3.1.tar.gz
 tar -xf lastpass-cli-1.3.1.tar.gz
